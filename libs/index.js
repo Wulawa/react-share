174e9d725ff57d4fb0c9e1cdadc38476b7bc4bb1
LuFwulawa <785035727@qq.com>
2018-03-22 18:40:01 +0800

    init

---
+++ libs/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Share extends Component {
+  render() {
+    return (
+      <div>
+        aaa
+      </div>
+    )
+  }
+}
+
+export default Share
